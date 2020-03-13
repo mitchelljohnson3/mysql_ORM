563d306c54d03be09162e9357460ca17e7c6e612
Mitchell-Johnson <mitchj9@uw.edu>
2020-03-12 17:45:39 -0700

    test

--- mysql_orm.js
+++ mysql_orm.js
@@ -4,6 +4,9 @@
 const mysql = require('mysql');
 
 class orm {
+
+    // constructor creates the connection object
+    // using the mysql module
     constructor(database_name) {
         this.database_name = database_name;
         this.connection = mysql.createConnection({
@@ -14,50 +17,130 @@ class orm {
             database: this.database_name
         });
     }
-    
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // ORM FUNCTIONALITY METHODS
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    // creates connection to the database
     init() {
         this.connection.connect((err) => {
-            if(err) throw err;
+            if (err) throw err;
             console.log(`connection established with ${this.database_name}`);
         });
     }
 
+    // kills the connection to the database
     kill() {
         console.log(`connection ended with ${this.database_name}`);
         this.connection.end();
     }
 
-    select_all_from_one_table(table_name) {
+    // returns promise based query
+    db_query(query, values) {
+        values = values ? values : [];
         return new Promise((resolve, reject) => {
-            const query = 'select * from ??';
             this.connection.query(
                 query,
-                [table_name],
+                values,
                 (err, results) => {
                     if (err) {
                         reject(err);
                     }
-                    resolve(results);
+                    resolve(this.format(results));
                 }
             );
         });
     }
 
-    select_all_from_one_table_where_one_column_equals_value(table_name, parameter, value) {
-        return new Promise((resolve, reject) => {
-            const query = 'select * from ?? where ?? = ?';
-            this.connection.query(
-                query,
-                [table_name, parameter, value],
-                (err, results) => {
-                    if (err) {
-                        reject(err);
-                    }
-                    resolve(results);
-                }
-            );
+    // this method removes the 'RowDataPacket' indicator off of the objects
+    // retrieved from the database
+    // every query uses the function by default
+    format(data) {
+        return data.map(object => {
+            const newObj = {};
+            const keys = Object.keys(object);
+            const values = Object.values(object);
+            for (let i = 0; i < keys.length; i++) {
+                newObj[keys[i]] = values[i];
+            }
+            return newObj;
         });
     }
+
+    // this function converts the returned queries into arrays
+    // it takes a boolean parameter, true to remove values, false to remove keys
+    // the 'data' variable is the data returned from a query
+    // it is optional, only used if the user desires to
+    formatToArray(data, bool) {
+        return data.map(object => {
+            return bool ? Object.keys(object) : Object.values(object);
+        });
+    }
+
+    // this function filters out unwanted values from queries
+    // it takes an array as a parameter which contains the
+    // column names that the user wants returned, all other
+    // columns will be 'filtered' out by this method
+    // the 'data' variable is the data returned from a query
+
+    // NOTE! THIS FUNCTION DEPENDS ON THE QUERIES BEING IN OBJECT FORM
+    // IT WILL THROW AN ERROR IF YOU USE THE formatToArray METHOD BEFORE THIS ONE
+    filter(data, filterArray) {
+        return data.map(object => {
+            const newObj = {};
+            const keys = Object.keys(object);
+            const values = Object.values(object);
+            for (let i = 0; i < keys.length; i++) {
+                if(filterArray.includes(keys[i])) newObj[keys[i]] = values[i];
+            }
+            return newObj;
+        });
+    }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // QUERIES
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    // user define raw query
+    raw(query) {
+        return this.db_query(query, null);
+    }
+
+    // all rows from table
+    select_all_from_table(table_name) {
+        const query = 'select * from ??';
+        const values = [table_name];
+        return this.db_query(query, values);
+    }
+
+    // one column from table
+    select_column_from_table(column_name, table_name) {
+        const query = 'select ?? from ??';
+        const values = [column_name, table_name];
+        return this.db_query(query, values);
+    }
+
+    // rows from table based on a columns value
+    select_all_from_table_where_column_equals_value(table_name, colum_name, value) {
+        const str = (value == null) ? 'is null' : '= ?';
+        const query = `select * from ?? where ?? ${str}`;
+        let values = [table_name, colum_name];
+        if (value != null) values.push(value);
+        return this.db_query(query, values);
+    }
+
+    // +++++
+    // JOINS
+    // +++++
+
+    // inner join table with another table on primary key
+    table1_inner_join_table2_ON_table1_key_equals_table2_key(table1_name, table2_name, table1_key, table2_key) {
+        const query = 'select * from ?? inner join ?? on ??.?? = ??.??';
+        const values = [table1_name, table2_name, table1_name, table1_key, table2_name, table2_key];
+        return this.db_query(query, values);
+    }
+
 }
 
 module.exports = orm;
--- test_script.js
+++ test_script.js
@@ -8,14 +8,27 @@ const mysql = require('mysql');
 const orm = new db_orm('cms_database');
 orm.init();
 
-// below are sample functions that are commented out
+// this function ends the connection to the database
+// orm.kill();
+
+// this function converts the returned queries into arrays
+// it takes a boolean parameter, true to remove values, false to remove keys
+// it is optional, only used if the user desires
+// the 'data' variable is the response from an ORM database query
+// orm.formatToArray(data, true);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+// SAMPLE FUNCTIONS
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+
+// below are sample functions that are commented out that
+// demonstrate the usage of this ORM
 // uncomment the functions to test them
 
 // ================================================================
-//  this function takes the name of the table, and returns
-// all the data contained within it
+// returns all data from one table
 // ================================================================
-// orm.select_all_from_one_table('department')
+// orm.select_all_from_table('department')
 // .then((data) => {
 //     console.log(data);
 // }).then(() => {
@@ -24,13 +37,64 @@ orm.init();
 // ================================================================
 
 // ================================================================
-// this function like the name suggests, selects all the
-// data from one table where one column equald one value
+// this function returns one column from the from a table
 // ================================================================
-// orm.select_all_from_one_table_where_one_column_equals_value('employee', 'manager_id', 1)
+// orm.select_column_from_table('first_name', 'employee')
 // .then((data) => {
 //     console.log(data);
 // }).then(() => {
 //     orm.kill();
 // });
 // ================================================================
+
+// ================================================================
+// returns all rows from table where one column equals a value
+// ================================================================
+// orm.select_all_from_table_where_column_equals_value('employee', 'manager_id', null)
+// .then((data) => {
+//     console.log( orm.formatToArray( (orm.filter(data, ['first_name', 'last_name']) ) ) );
+// }).then(() => {
+//     orm.kill();
+// });
+// ================================================================
+
+// ================================================================
+// this table joins two tables together on their primary keys
+// ================================================================
+// orm.table1_inner_join_table2_ON_table1_key_equals_table2_key('employee', 'role', 'role_id', 'id')
+// .then((data) => {
+//     console.log(data);
+// }).then(() => {
+//     orm.kill();
+// });
+// ================================================================
+
+// ================================================================
+// this function lets the user make a raw query to the database
+// ================================================================
+// orm.raw('select * from department')
+// .then((data) => {
+//     console.log(data);
+// }).then(() => {
+//     orm.kill();
+// });
+// ================================================================
+
+// ================================================================
+// 
+// ================================================================
+// ================================================================
+
+// ================================================================
+// 
+// ================================================================
+// ================================================================
+
+// ================================================================
+// 
+// ================================================================
+// ================================================================
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
